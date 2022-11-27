aac5e5453770bbcd52afb308578cdeafe21f7c92
khai <thekhai1k9@gmail.com>
2022-11-28 00:33:20 +0700

    update

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Tâm ei Tâmmmmm à à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Tâmmm cứ đi nhầm đường
+    Lạc vào tim mình lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng minh đã yêu ngay từ cái nhìn đầu tiên =)))) ',
+    desc: 'Phải chăng mình đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Noel này mình đi dạo phố nhasaaaaa... :<<. =))))',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/ptkhai99/' 
+}
